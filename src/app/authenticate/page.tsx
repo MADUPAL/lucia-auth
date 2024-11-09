d8ca87a8aab3a4b7fdda3623debabcd8e6f22be9
MADUPAL <kevin488@naver.com>
2024-11-09 22:53:05 +0900

    add shadcn

---
+++ src/app/authenticate/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AuthenticatePage = () => {
+  //2753
+  return (
+    <div className="relative flex w-full h-screen bg-background">
+      <div className="max-w-3xl absolute left-1/2 top-1/2 -translatex-1/2 -translate-y-1/2"></div>
+    </div>
+  );
+};
+
+export default AuthenticatePage;
