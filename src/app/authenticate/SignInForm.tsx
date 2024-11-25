ac4f778f25899e662198ed028578d2488896da6a
MADUPAL <kevin488@naver.com>
2024-11-25 23:12:46 +0900

    signin

---
+++ src/app/authenticate/SignInForm.tsx
@@ -0,0 +1,103 @@
+"use client";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import React from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { z } from "zod";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const signInSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+});
+
+const SignInForm = () => {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof signInSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle> Welcome Back! </CardTitle>
+        <CardDescription>Sign in to your account to continue</CardDescription>
+      </CardHeader>
+      <CardContent className="space-y-2">
+        <Form {...form}>
+          <form
+            className="flex flex-col gap-2"
+            onSubmit={form.handleSubmit(onSubmit)}
+          >
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="email"
+                      placeholder="Enter your email..."
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="Enter your password..."
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button typeof="submit" className="self-start">
+              Login
+            </Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  );
+};
+// 3950
+export default SignInForm;
--- src/app/authenticate/page.tsx
+++ src/app/authenticate/page.tsx
@@ -1,10 +1,14 @@
+import TabSwitcher from "@/components/TabSwitcher";
 import React from "react";
+import SignInForm from "./SignInForm";
 
 const AuthenticatePage = () => {
-  //2753
+  //2900
   return (
     <div className="relative flex w-full h-screen bg-background">
-      <div className="max-w-3xl absolute left-1/2 top-1/2 -translatex-1/2 -translate-y-1/2"></div>
+      <div className="max-w-3xl absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+        <TabSwitcher SignInTab={<SignInForm />} SignUpTab={<h1>Sign up</h1>} />
+      </div>
     </div>
   );
 };
